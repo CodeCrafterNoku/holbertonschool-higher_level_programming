74b3decc4d0fef81449ba2fca63185ae497cfa03
CodeCrafterNoku <nokubongangema8@gmail.com>
2025-06-23 11:12:20 +0000

    happy coding

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    
+    for i in range(list_length):
+        try:
+            element_1 = my_list_1[i]
+            element_2 = my_list_2[i]
+            result.append(element_1 / element_2)
+        except IndexError:
+            print("out of range")
+            result.append(0)
+        except TypeError:
+            print("wrong type")
+            result.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            result.append(0)
+        finally:
+            continue
+            
+    return result
