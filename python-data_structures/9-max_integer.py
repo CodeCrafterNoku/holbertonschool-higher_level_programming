624d75b436ff1c34755c9eb872a1e995b09339be
CodeCrafterNoku <nokubongangema8@gmail.com>
2025-06-23 08:31:14 +0000

    happy coding

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,13 +3,10 @@ def max_integer(my_list=[]):
     # Return None if the list is empty
     if not my_list:
         return None
-    
     # Initialize max_value with the first element of the list
     max_value = my_list[0]
-    
     # Iterate through the list to find the maximum value
     for num in my_list:
         if num > max_value:
             max_value = num
-    
     return max_value
