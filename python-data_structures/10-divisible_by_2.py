5771f385f0b6465a9a7ddf1106e012a751f971a5
CodeCrafterNoku <nokubongangema8@gmail.com>
2025-06-23 08:35:32 +0000

    happy coding

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,10 +2,10 @@
 def divisible_by_2(my_list=[]):
     # Create a new list to hold the results
     result = []
-    
+
     # Iterate through the original list
     for num in my_list:
         # Check if the number is divisible by 2
         result.append(num % 2 == 0)
-    
+
     return result
