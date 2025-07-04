2a0824fe0dac8abb4b1c1010c7548aa71fb543a6
CodeCrafterNoku <nokubongangema8@gmail.com>
2025-07-04 09:49:33 +0000

    happy coding

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -10,5 +10,7 @@ class MyList(list):
     """
 
     def print_sorted(self):
-        """Prints the list in sorted ascending order"""
-        print(sorted(self))
+        """Prints and returns the list in sorted ascending order"""
+        sorted_list = sorted(self)
+        print(sorted_list)
+        return sorted_list
