7e7a484cd7891d6bb5772150d02f91e1627f9d5d
Nokubonga Ngema <156459137+CodeCrafterNoku@users.noreply.github.com>
2025-07-04 13:42:48 +0200

    Update 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-"""
-Module containing function to read and print file contents
-"""
+"""a function that reads a text file (UTF8) and pirnts it to stdout"""
 
 
 def read_file(filename=""):
-    """
-    Reads a text file and prints its contents to stdout
-    
-    Args:
-        filename: name of the file to read
-    """
-    with open(filename, encoding='utf-8') as f:
-        print(f.read(), end="")
+    """read 'filename'"""
+
+    with open(filename, "r", encoding='utf-8') as a_file:
+        print("{}".format(a_file.read()), end="")
