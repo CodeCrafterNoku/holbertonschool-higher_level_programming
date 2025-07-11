18593c0df0bfa86346929f53de087d74f020f873
CodeCrafterNoku <nokubongangema8@gmail.com>
2025-07-11 08:04:39 +0000

    happy coding

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -1,13 +1,11 @@
-"""
-basic flask app that serves a webpage
-"""
+import os
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=os.path.abspath('templates'))
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
