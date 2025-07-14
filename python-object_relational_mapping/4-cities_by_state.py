0239755502958315f6954b7e2fdbc5014067617e
CodeCrafterNoku <nokubongangema8@gmail.com>
2025-07-14 08:16:48 +0000

    happy coding

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,17 +1,39 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+"""Lists all cities and their corresponding state names from hbtn_0e_4_usa"""
+
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("""SELECT cities.id, cities.name, states.name FROM
-                cities INNER JOIN states ON states.id=cities.state_id""")
-    rows = cur.fetchall()
-    for row in rows:
+    # Get CLI args
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # Connect to DB
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+
+    # Cursor
+    cursor = db.cursor()
+
+    # Execute JOIN query: city id, city name, state name
+    cursor.execute("""
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        ORDER BY cities.id ASC
+    """)
+
+    # Display results
+    for row in cursor.fetchall():
         print(row)
-    cur.close()
+
+    # Cleanup
+    cursor.close()
     db.close()
